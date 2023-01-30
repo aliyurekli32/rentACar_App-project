924e1d7460716ea401f44ededfecbd9b30e04747
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-30 17:35:34 +0300

    add: reservation

--- car/urls.py
+++ car/urls.py
@@ -2,12 +2,12 @@
 from django.urls import include, path
 
 from rest_framework import routers
-from .views import CarView
+from .views import CarView, ReservationView
 
 router = routers.DefaultRouter()
 router.register('car',CarView)
 urlpatterns = [
-
+    path('reservation/', ReservationView.as_view())
 ]
 
 urlpatterns += router.urls
