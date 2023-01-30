6de696a0c83f4cc8f273230770e93d0591b61c7a
aliyurekli32 <adamjesperblack@gmail.com>
2023-01-30 17:35:22 +0300

    update: serializers

--- car/serializers.py
+++ car/serializers.py
@@ -23,3 +23,27 @@ class CarSerializers(serializers.ModelSerializer):
             fields.pop('availability')
             fields.pop('plate_number')
         return fields
+    
+class ReservationSerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField()
+    
+    
+    class Meta:
+        model = Reservation
+        fields = (
+            'id',
+            'customer',
+            'car',
+            'start_date',
+            'end_date',
+            'total_price',
+        )
+        validators = [
+            serializers.UniqueTogetherValidator(
+                queryset= Reservation.objects.all(),
+                fields = ('customer', 'start_date', 'end_date'),
+                message= ('You already have a reservation between theese dates...'),
+            )
+        ]
+    def get_total_price(self, obj):
+        return obj.car.rent_per_day * (obj.end_date - obj.start_date).days
